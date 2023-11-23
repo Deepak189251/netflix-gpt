05db23b7298fcd44970fba80ab4568f2568a3040
D <deepakkumarbehera2002@gmail.com>
2023-11-23 09:57:46 +0530

    resized logo and signin button on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 
 const Header = () => {
     return (
-       <div className=" absolute">
-          <div className="logo-container ">
-            <img className=" w-48" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
+       <div className=" absolute flex justify-between w-[100%]">
+          <div className="logo-container ml-[140px] mt-[7px]">
+            <img className=" w-44 h-18" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
           </div>
-          <div className="signin-container">
-             <button>Sign In</button>
+          <div className="signin-container mr-[160px] mt-[15px]">
+             <button className=" rounded-md text-sm text-white w-[70px] h-8 bg-red-600">Sign In</button>
           </div>
        </div>
     ) 
