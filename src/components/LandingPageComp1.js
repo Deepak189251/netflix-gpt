686314cb387877993b25f1eebf4bf122a80022d2
D <deepakkumarbehera2002@gmail.com>
2023-12-06 22:05:03 +0530

    created new component in landing page

---
+++ src/components/LandingPageComp1.js
@@ -0,0 +1,21 @@
+const LandingPageComp1 = () => {
+
+    return (
+        <div className=" text-white">
+          <div className="comp1-container w-[1381px] h-[582px] pt-[72px] pb-[72px]" >
+           <div className=" w-[1061px] h-[438px] bg-red-400 ml-[160px] mr-[160px] flex">
+              <div className=" w-[525px] pr-[12px] pt-[154px] pb-[154px]">
+                 <h1 className=" text-[43px] font-bold">Enjoy on your TV</h1>
+                 <p className=" text-[22px]">Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
+              </div>
+              <div className=" w-[525px] pl-[10px]">
+              <img className=" h-[434px]" alt="tv" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" data-uia="nmhp-card-animation-asset-image" class="default-ltr-cache-1d3w5wq" />
+              </div>
+           </div> 
+          </div>
+            <div className=" divider w-[1381px] h-[8px] bg-[rgb(35,35,35)]"></div>
+        </div>
+    )
+}
+
+export default LandingPageComp1
