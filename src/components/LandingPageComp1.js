53bc5d7a8d1e4750c7e11e3deb6c3a6661a82cee
D <deepakkumarbehera2002@gmail.com>
2023-12-11 22:30:55 +0530

    created new section in landing page

--- src/components/LandingPageComp1.js
+++ src/components/LandingPageComp1.js
@@ -1,16 +1,16 @@
 const LandingPageComp1 = () => {
 
     return (
-        <div className=" text-white">
-          <div className="comp1-container bg-black w-[1381px] h-[582px] pt-[72px] pb-[72px]" >
+        <div className="  text-white">
+          <div className="container-comp1 bg-black w-[1381px] h-[538px] pt-[50px] pb-[50px]" >
            <div className=" w-[1061px] h-[438px]  ml-[160px] mr-[160px] flex">
               <div className=" w-[525px] pr-[12px] pt-[154px] pb-[154px]">
-                 <h1 className=" text-[43px] font-bold">Enjoy on your TV</h1>
+                 <h1 className=" text-[43px] font-extrabold">Enjoy on your TV</h1>
                  <p className=" text-[22px]">Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
              </div >
              <div className=" w-[525px] pl-[10px] relative">
                <img className= " h-[394px] w-[519px] bottom-[19px] z-[1] absolute" alt="tv" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" data-uia="nmhp-card-animation-asset-image"/>
-               <video className=" absolute w-[374px] bottom-[52px] left-[80px] h-[211px] top-[109px]" data-uia="nmhp-card-animation-asset-video" autoplay="" playsinline="" muted="" loop=""><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v" type="video/mp4"/></video>
+               <video className=" absolute w-[374px] bottom-[52px] left-[80px] h-[211px] top-[109px]" data-uia="nmhp-card-animation-asset-video" autoPlay muted loop ><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v" type="video/mp4"/></video>
                 
             
               </div>
