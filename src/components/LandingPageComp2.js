2b6e41f1aeed80d4f96b2ad86ee08eb71afe4ccb
D <deepakkumarbehera2002@gmail.com>
2023-12-12 22:20:29 +0530

    added a new component to the landing page

---
+++ src/components/LandingPageComp2.js
@@ -0,0 +1,34 @@
+const LandingPageComp2 = () => {
+
+    return (
+        <div className="">
+            <div className=" container-comp2 bg-black text-white  w-[1381px] h-[538px] pt-[50px] pb-[50px]">
+                <div className=" w-[1061px] h-[438px]  ml-[160px] mr-[160px] flex">
+                    <div className="w-[525px] relative">
+                       <img className=" h-[434px] absolute" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" data-uia="nmhp-card-animation-asset-image" class="default-ltr-cache-1d3w5wq" />
+                       <div className=" w-[290px] h-[100px]  bg-black absolute top-[310px] left-[115px] py-[8px] px-[12px] rounded-md flex border-[2px] border-[rgba(128,128,128,0.7)">
+                           <div className=" h-[84px]">
+                              <img className=" h-[84px]" data-uia="nmhp-card-animation-asset-animation" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png" class="default-ltr-cache-1t1z3a4"/>
+                           </div>
+                           <div className=" ml-[14px] mt-[22px] w-[168px]  h-[40px]"> 
+                              <h1 className=" text-[16px] font-semibold text-white">Stranger Things</h1>
+                              <p className=" text-xs text-blue-600">Downloading...</p>
+                           </div>
+                           <div className=" w-[48px] h-[50px] mt-[18px]"> 
+                              <img className=" w-[100%] h-[100%] " src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/download-icon.gif" alt="download-icon-gif"/>
+                           </div>
+                       </div>
+                    </div>
+                    <div className="w-[525px] mt-[95px] ">
+                        <h1 className=" text-[43px] font-extrabold text-white">Download your shows <br/> to watch offline</h1>
+                        <p className=" text-[21px] font-medium ">Save your favourites easily and always have something <br/> to watch.</p>
+                    </div>
+                </div>                
+            </div>
+            <div className=" divider w-[1381px] h-[8px] bg-[rgb(35,35,35)]"></div>
+        </div>
+    )
+
+}
+
+export default LandingPageComp2
