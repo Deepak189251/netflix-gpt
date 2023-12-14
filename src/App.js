355748de7fc1cb3899e7fcca724f195495ea507e
D <deepakkumarbehera2002@gmail.com>
2023-12-14 22:04:36 +0530

    Added app layout in the app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Login from "./components/LogIn";
 import Browse from "./components/Browse";
+const Applayout = () => {
+  
+  return(
+    <div className="app">
+       <Outlet/>
+    </div>
+  )
+}
+
+
 function App() {
   const appRouter = createBrowserRouter([
     {
       path: "/",
-      element: <LandingPage />,
+      element: <Applayout />,
       children : [
-         {
-          path: "/Login",
+        {
+          path: "/",
+          element: <LandingPage />
+        },
+        {
+          path: "/login",
           element: <Login />
-         }
+        }
       ]
     },
     {
