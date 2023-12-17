4fbac3422ee8188c7a83e3d2a63ea53c242c9289
D <deepakkumarbehera2002@gmail.com>
2023-12-17 22:22:28 +0530

    added a new section to the landing page

---
+++ src/components/LandingPageComp4.js
@@ -0,0 +1,23 @@
+const LandingPageComp4 = () => {
+
+    return(
+        <div>
+            <div className="container-comp4 text-white bg-black w-[1381px] h-[538px] pt-[50px] pb-[50px]">
+                <div className=" w-[1061px] h-[438px]  ml-[160px] mr-[160px] flex">
+                    <div className="w-[525px]">
+                    <img className=" w-[100%] h-[434px]" src="https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVr8nYuAg0xDpXDv0VI9HUoH7r2aGp4TKRCsKNQrMwxzTtr-NlwOHeS8bCI2oeZddmu3nMYr3j9MjYhHyjBASb1FaOGYZNYvPBCL.png?r=54d" alt=""/>
+                    </div>
+                    <div className="w-[525px] pl-[6px] h-[199px] pb-[24px] mt-[110px]">
+                        <h1 className=" text-[43px] font-extrabold">Create profiles for kids</h1>
+                        <p className="text-[21px] font-medium pt-[16px]">Send children on adventures with their favourite characters in a space made just for them—free with your membership.</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className=" divider w-[1381px] h-[8px] bg-[rgb(35,35,35)]"></div>
+        </div>
+    )
+}
+
+export default LandingPageComp4
+
