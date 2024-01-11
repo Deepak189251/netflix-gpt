9a3e49582b1d86792a7b28c774fc5a70d371cff0
D <deepakkumarbehera2002@gmail.com>
2024-01-11 22:51:14 +0530

    debugged the language selector in login and footer

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGlobe } from "@fortawesome/free-solid-svg-icons"
+
 const Login = () => {
     return (
 
@@ -50,11 +53,11 @@ const Login = () => {
                   </div>
                </div>
                <div className=" justify-between relative mt-[30px] ">
-                   <svg className=" text-white absolute top-[5px] left-[5px] pl-[5px]" alt=""  width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" class="default-ltr-cache-4z3qvp e1svuwfo1" data-name="Languages" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.7668 5.33333L10.5038 5.99715L9.33974 8.9355L8.76866 10.377L7.33333 14H9.10751L9.83505 12.0326H13.4217L14.162 14H16L12.5665 5.33333H10.8278H10.7668ZM10.6186 9.93479L10.3839 10.5632H11.1036H12.8856L11.6348 7.2136L10.6186 9.93479ZM9.52722 4.84224C9.55393 4.77481 9.58574 4.71045 9.62211 4.64954H6.41909V2H4.926V4.64954H0.540802V5.99715H4.31466C3.35062 7.79015 1.75173 9.51463 0 10.4283C0.329184 10.7138 0.811203 11.2391 1.04633 11.5931C2.55118 10.6795 3.90318 9.22912 4.926 7.57316V12.6667H6.41909V7.51606C6.81951 8.15256 7.26748 8.76169 7.7521 9.32292L8.31996 7.88955C7.80191 7.29052 7.34631 6.64699 6.9834 5.99715H9.06968L9.52722 4.84224Z" fill="currentColor"></path></svg>
-                   <select className=" bg-[rgb(45,45,45)] text-white pl-[22px] border border-1px-white  mr-[22px] text-sm rounded  pb-[5px] pr-[8px] pt-[2px]">
+                   <FontAwesomeIcon icon={faGlobe} color="white" className="text-white absolute top-[5px] left-[5px] pl-[5px] mr-[5px] mt-[4px]" />
+                   <select className=" bg-black text-white pl-[29px] border border-1px-white  mr-[22px] rounded  pb-[5px] pr-[8px] pt-[6px] font-light text-xs">
                 
-                      <option className=" bg-white">English </option>  
-                      <option className=" bg-white">Hindi </option>
+                      <option className=" bg-white font-light text-xs">English </option>  
+                      <option className=" bg-white font-light text-xs">Hindi </option>
                    </select> 
                </div>
               </div>
