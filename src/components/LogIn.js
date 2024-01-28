d0d9bc1cefff4c918920cc95bc55d8af68971288
D <deepakkumarbehera2002@gmail.com>
2024-01-28 22:01:39 +0530

    commit did a bug fix in login component

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,23 +1,71 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate} from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGlobe } from "@fortawesome/free-solid-svg-icons"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../utils/Firebase"
+import { useRef } from "react"
+import { useDispatch } from "react-redux"
+import { addUser } from "../utils/UserSlice"
+//import { UserState } from "../utils/Context"
 
 const Login = () => {
+   const dispatch = useDispatch()
+  //const {state:{users}, dispatch} = UserState()
+  const navigate = useNavigate()
+   const Email = useRef(null);
+   const Password = useRef(null);
+   
+
+   const handleLogin = () => {
+ 
+      signInWithEmailAndPassword(auth, Email.current.value, Password.current.value)
+         .then((userCredential) => {
+         // Signed in 
+         console.log("signin sucessfully")
+           const user = userCredential.user;
+            // const {uid, email} = user
+
+          /*   dispatch({
+               type: "Add",
+               payload: user
+            }) */
+            
+           // console.log(user)
+            navigate("/browse")
+            dispatch(addUser()) 
+           
+            console.log(user)
+
+            // ...
+         })
+         .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log( errorCode + errorMessage)
+         });
+
+   }
+   
+
+  /* const handleLogin = () => {
+
+      console.log()
+
+   }*/
+
     return (
 
       <div>
-        <div className=" w-[1381px] h-[920px] bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/c906271d-7184-4eec-83c9-b6d4c1a068ec/728874a6-eeda-400a-9bcf-a935a1408a4f/IN-en-20231127-popsignuptwoweeks-perspective_alpha_website_large.jpg')]">
+        <div className=" w-[100%]  h-[958px] bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/c906271d-7184-4eec-83c9-b6d4c1a068ec/728874a6-eeda-400a-9bcf-a935a1408a4f/IN-en-20231127-popsignuptwoweeks-perspective_alpha_website_large.jpg')]">
            <div className=" w-[100%] h-[100%] bg-gradient-to-t from-[rgba(0,0,0,0.7)] via-[rgba(0,0,0,0.4)] to-[rgba(0,0,0,0.7)]">
-             <header className="login-header">
-                <img className=" w-44 h-18 ml-[20px]" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" /> 
-             </header>
-             <div className=" w-[430px] h-[660] px-[68px] py-[60px] ml-[475px] bg-[rgba(0,0,0,.75)] rounded-sm">
-                <div className="  text-white">
+            
+             <div className=" absolute w-[430px] top-[140px] h-[660] px-[68px] py-[60px] left-[560px] bg-[rgba(0,0,0,.75)] rounded-sm">
+                <div className="   text-white">
                    <h1 className=" text-[30px] font-medium mb-[28px]">Sign In</h1>
                    <form>
-                     <input className=" w-[100%] h-[50px] mb-[16px] pl-[10px] rounded placeholder-[#8c8c8c] bg-[#333]" type="Email" placeholder="Email or phone number"></input>
-                     <input className=" w-[100%] h-[50px] mb-[16px] pl-[10px] rounded placeholder-[#8c8c8c] bg-[#333]" type="password" placeholder="Password"></input>
-                     <button className=" w-[100%] h-[50px] mb-[16px] rounded bg-red-600 hover:bg-[rgb(193,17,25)] duration-300" type="submit">Sign In</button>
+                     <input className=" w-[100%] h-[50px] mb-[16px] pl-[10px] rounded placeholder-[#8c8c8c] bg-[#333]" ref={Email}  type="Email" placeholder="Email or phone number"></input>
+                     <input className=" w-[100%] h-[50px] mb-[16px] pl-[10px] rounded placeholder-[#8c8c8c] bg-[#333]" ref={Password}  type="password" placeholder="Password"></input>
+                     <Link><button onSubmit={(e) => e.preventDefault} onClick={handleLogin} className=" w-[100%] h-[50px] mb-[16px] rounded bg-red-600 hover:bg-[rgb(193,17,25)] duration-300" >Sign In</button></Link>
                      
                      <div className=" w-[100%] flex justify-between">
                        <div >
@@ -36,7 +84,7 @@ const Login = () => {
             
  
              </div>
-             <footer className=" w-[100%] h-[250px] bg-[rgba(0,0,0,.75)] text-[#737373] mt-[80px]">
+             <footer className=" absolute w-[100%] h-[250px] bg-[rgba(0,0,0,.75)] text-[#737373] mt-[80px] top-[630px]">
                <div className="w-[1000px] pt-[30px] pb-[30px] mr-[259px] ml-[259px]">
                   <div className="1st-row pb-[15px]">Questions? Call 000-800-919-1694</div>
                   <div className="footer-body">
