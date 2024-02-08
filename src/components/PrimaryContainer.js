a13ca901c620a2b4fa5c151c5373d583f95caa5c
D <deepakkumarbehera2002@gmail.com>
2024-02-08 22:17:36 +0530

    created primary container inside browse for main trailer

---
+++ src/components/PrimaryContainer.js
@@ -0,0 +1,25 @@
+import {  useSelector } from "react-redux"
+import VideoBackground from "./VidoBackground"
+import VideoTitle from "./VideoTitle"
+
+const PrimaryContainer = () => {
+      
+    const Movie = useSelector((store) => (store.movie?.nowMovie))
+
+    if(!Movie) return
+    
+    //let n = Math.floor(Math.random()*Movie.length) 
+   // console.log(n)
+    const MainMovie = Movie[3]
+    const {title, overview, id} = MainMovie
+    //console.log(original_title)
+
+    return (
+        <div>
+            <VideoTitle title={title} overview={overview} />
+            <VideoBackground id={id} />
+        </div>
+    )
+}
+
+export default PrimaryContainer
