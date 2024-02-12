64216299fb72f8d68926feebdd6fb175ed054db9
D <deepakkumarbehera2002@gmail.com>
2024-02-12 23:02:56 +0530

    added different language in the webpage.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,7 +7,14 @@
  import Footer from "./Footer";
  import {SlArrowRight} from "react-icons/sl";
  import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { lang } from "../utils/LanguageConstants";
+//import { useEffect, useState } from "react";
  const LandingPage = () => {
+   
+  const selectedLanguage = useSelector(store => store?.config?.preferedLanguage)
+  
+
     return (
    <div className="landingpage-container w-[1519px]">
      <div className=" bg-center bg-cover w-[100%] h-[630px] bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg')]">
@@ -18,21 +25,21 @@
         <div className=" flex  absolute mt-[250px] justify-center w-[100%]">
           <div className=" text-white  text-center ">
             <div>
-              <h1 className=" text-[43px] font-extrabold">Laughter. Tears. Thrills. Find it all on Netflix.</h1>
-              <p className=" mt-[13px] text-2xl font-normal">Endless entertainment starts at just ₹149. Cancel anytime.</p>
+              <h1 className=" text-[43px] font-extrabold">{lang[selectedLanguage].landingHeader}</h1>
+              <p className=" mt-[13px] text-2xl font-normal">{lang[selectedLanguage].landingHeader2}</p>
             </div>
             {/**Laughter. Tears. Thrills. Find it all on Netflix.    ml-[220px] mr-[220px]*/}
             <div className=" text-white mt-[16px]" >
-              <p className=" text-xl font-normal">Ready to watch? Enter your email to create or restart your membership.</p>
+              <p className=" text-xl font-normal">{lang[selectedLanguage].landingHeader3}</p>
              
               <form className=" flex justify-between mt-[13px]">
              
                  
                  <div className="">
-                    <input type="email" className=" mb-[5px]  bg-black w-[374px] h-[56px] ml-[190px] border rounded text-left pb-2 pl-3" placeholder="Email address"></input>
+                    <input type="email" className=" mb-[5px]  bg-black w-[374px] h-[56px] text-white-[20px] border rounded text-left pb-2 pl-3" placeholder="Email address"></input>
                  </div>  
             
-               <Link to={"/signup-form"}><button type="submit" className=" bg-red-600 w-[203px] h-[56px] mr-[180px] text-2xl font-semibold rounded text-left pl-3 flex pt-2 hover:bg-[rgb(193,17,25)] duration-300">Get started <SlArrowRight className=" pl-[10px] pt-[6px] text-3xl font-extrabold"/> </button></Link> 
+               <Link to={"/signup-form"}><button type="submit" className=" bg-red-600 w-[203px] h-[56px] mr-[180px] text-2xl font-semibold rounded text-left pl-3 flex pt-2 hover:bg-[rgb(193,17,25)] duration-300">{lang[selectedLanguage].signupBtn}<SlArrowRight className=" pl-[10px] pt-[6px] text-3xl font-extrabold"/> </button></Link> 
               </form>
             </div>
          </div>
