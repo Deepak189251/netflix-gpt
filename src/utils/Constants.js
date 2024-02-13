766e48fbd807543c265352523a472cdabb4a7727
D <deepakkumarbehera2002@gmail.com>
2024-02-13 22:57:52 +0530

    added supported language in constatnt

---
+++ src/utils/Constants.js
@@ -0,0 +1,31 @@
+export const Netflix_Logo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+
+export const Background_Img = "https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+
+export const nowMovieUrl = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+
+export const topRatedMovieUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1'
+
+export const popularMovieUrl = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+
+export const upcomingMovieUrl = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1'
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzBlMTQwYTIwNWEwMGY0NjYwNzM3MjVlOWVjZWE3MiIsInN1YiI6IjY1YmRhZWRmZmM2NTM4MDE2M2U4ZDlkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dpjWR9CLj74S9OKCxsZ9AKjX0_2MIHiyUwZgrD3Y5a4'
+    }
+  };
+
+export const SupportedLanguage = [
+  {
+    identifier: "english",
+    name: "English"
+  },
+  {
+    identifier: "hindi",
+    name: "हिंदी"
+  }
+  
+]
