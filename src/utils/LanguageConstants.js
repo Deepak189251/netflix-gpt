31c47cbe80ce2e40dc77c6800b6ed9a95f92a1ba
D <deepakkumarbehera2002@gmail.com>
2024-03-06 22:57:42 +0530

    added language constants

---
+++ src/utils/LanguageConstants.js
@@ -0,0 +1,44 @@
+export const lang = {
+
+    english: {
+        landingHeader: "Laughter. Tears. Thrills. Find it all on Netflix.",
+        landingHeader2: "Endless entertainment starts at just ₹149. Cancel anytime.",
+        landingHeader3: "Ready to watch? Enter your email to create or restart your membership.",
+        signupBtn: "Get started",
+        signupInput: "Email address",
+        signinBtn: "Sign In",
+        landingComp1Header: "Enjoy on your TV",
+        landingComp1Para: "Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.",
+        landingComp2Header: "Download your shows to watch offline",
+        landingComp2Para: "Save your favourites easily and always have something to watch.",
+        landingComp2Title: "Downloading...",
+        landingComp3Header: "Watch everywhere",
+        landingComp3Para: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
+        landingComp4Header: "Create profiles for kids",
+        landingComp4Para: "Send children on adventures with their favourite characters in a space made just for them—free with your membership.",
+        FaqHeader: "Frequently Asked Questions",
+
+ 
+    },
+
+    hindi: {
+        landingHeader: "हँसी। आँसू। रोमांच. यह सब नेटफ्लिक्स पर खोजें।",
+        landingHeader2: "अंतहीन मनोरंजन मात्र ₹149 से शुरू होता है। किसी भी समय रद्द करें.",
+        landingHeader3: "देखने के लिए तैयार हैं? अपनी सदस्यता बनाने या पुनः आरंभ करने के लिए अपना ईमेल दर्ज करें।",
+        signupBtn: "शुरू हो जाओ",
+        signupInput: "मेल पता",
+        signinBtn: "दाखिल करना",
+        landingComp1Header: "अपने टीवी पर आनंद लें",
+        landingComp1Para: "स्मार्ट टीवी, PlayStation, Xbox, Chromecast, Apple TV, ब्लू-रे प्लेयर और बहुत कुछ पर देखें।",
+        landingComp2Header: "ऑफ़लाइन देखने के लिए अपने शो डाउनलोड करें",
+        landingComp2Para: "अपने पसंदीदा को आसानी से सहेजें और हमेशा देखने के लिए कुछ न कुछ रखें।",
+        landingComp2Title: "डाउनलोड हो रहा है...",
+        landingComp3Header: "हर जगह नजर रखें",
+        landingComp3Para: "अपने फ़ोन, टैबलेट, लैपटॉप और टीवी पर असीमित फिल्में और टीवी शो स्ट्रीम करें।",
+        landingComp4Header: "बच्चों के लिए प्रोफ़ाइल बनाएं",
+        landingComp4Para: "बच्चों को केवल उनके लिए बनाई गई जगह में उनके पसंदीदा पात्रों के साथ साहसिक यात्रा पर भेजें - आपकी सदस्यता के साथ निःशुल्क।",
+        FaqHeader: "अक्सर पूछे जाने वाले प्रश्नों",
+
+
+    }
+}
