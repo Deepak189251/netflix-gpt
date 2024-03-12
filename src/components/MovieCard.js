1abe92f931cd61023e049c5522d3d9e0ad3cdc13
D <deepakkumarbehera2002@gmail.com>
2024-03-12 22:35:05 +0530

    updated moviecard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 const MovieCard = ({poster}) => {
-
+     
+    if(!poster) return
     return (
         <div className=" mr-[20px]">
             <img className="w-[200px] h-[170px] max-w-[200px]" alt="poster" src={`https://image.tmdb.org/t/p/w500/${poster}`} /> 
