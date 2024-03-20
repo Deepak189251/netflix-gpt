14ccab6609f285eb20ab873d848cfb9cf31cd9de
D <deepakkumarbehera2002@gmail.com>
2024-03-20 23:35:49 +0530

    maded header responsice

--- src/components/Header.js
+++ src/components/Header.js
@@ -121,7 +121,7 @@ const Header = () => {
    return(
      
     <div   className={color ? colorT : colorF}>
-       <div className=" w-[100%] h-[100%] pr-[60px] pl-[50px]  flex  ">
+       <div className=" w-[100%] h-[100%] flex pr-[60px] pl-[50px]    ">
           <img className=" w-[117px] h-[58px] pt-[8px] mr-[35px]" src={Netflix_Logo} alt="logo" />
           <div className=" w-[100%] flex justify-between">
 
@@ -183,13 +183,13 @@ const Header = () => {
     return (
    
        <div className=" absolute flex justify-between w-[100%]">
-          <div className="logo-container ml-[140px] mt-[6px]">
-            <img className=" w-44 h-18" src={Netflix_Logo} alt="logo" />
+          <div className="logo-container mt-[6px] md:mt-[0px] ml-[20px]  sm:ml-[40px] md:ml-[80px]  xl:ml-[140px] ">
+            <img className=" w-[125px] h-[65px] md:w-[180px] md:h-[80px]" src={Netflix_Logo} alt="logo" />
           </div>
-          <div className="signin-container mr-[170px] mt-[25px] flex">
+          <div className="signin-container mt-[25px] flex mr-[20px] sm:mr-[40px] md:mr-[80px] xl:mr-[170px] ">
             <div className=" justify-between relative ">
               <IoLanguageOutline className="text-white mt-[3px] absolute top-[5px] left-[5px] pl-[5px]" />
-              <select onChange={handleToggleLanguage} className=" bg-[rgb(45,45,45)] text-white pl-[22px] border border-1px-white  mr-[22px] text-sm rounded h-[36px]  pb-[5px] pr-[8px] pt-[3px]">
+              <select onChange={handleToggleLanguage} className=" bg-[rgb(45,45,45)] text-white pl-[22px] border border-1px-white  md:mr-[22px] mr-[12px] text-sm rounded h-[31px] pr-[8px] sm:w-[96px] w-[46px]">
                 {SupportedLanguage.map(e => <option  key={e.identifier} value={e.identifier} className=" bg-white">{e.name}</option>)}
                 
               </select> 
@@ -197,7 +197,7 @@ const Header = () => {
                {/**             <svg className=" text-white absolute top-[5px] left-[5px] pl-[5px]" alt="" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.7668 5.33333L10.5038 5.99715L9.33974 8.9355L8.76866 10.377L7.33333 14H9.10751L9.83505 12.0326H13.4217L14.162 14H16L12.5665 5.33333H10.8278H10.7668ZM10.6186 9.93479L10.3839 10.5632H11.1036H12.8856L11.6348 7.2136L10.6186 9.93479ZM9.52722 4.84224C9.55393 4.77481 9.58574 4.71045 9.62211 4.64954H6.41909V2H4.926V4.64954H0.540802V5.99715H4.31466C3.35062 7.79015 1.75173 9.51463 0 10.4283C0.329184 10.7138 0.811203 11.2391 1.04633 11.5931C2.55118 10.6795 3.90318 9.22912 4.926 7.57316V12.6667H6.41909V7.51606C6.81951 8.15256 7.26748 8.76169 7.7521 9.32292L8.31996 7.88955C7.80191 7.29052 7.34631 6.64699 6.9834 5.99715H9.06968L9.52722 4.84224Z" fill="currentColor"></path></svg>
   */}
     
-            <button onClick={handleClick} className=" rounded text-xs px-[12px] py-[3px] text-white w-[70px] font-semibold bg-red-600 h-[38px] hover:bg-[rgb(193,17,25)] duration-300">{lang[selectedLanguage].signinBtn}</button>
+            <button onClick={handleClick} className=" rounded text-white w-[70px] font-semibold bg-red-600 h-[32px] hover:bg-[rgb(193,17,25)] duration-300"><p className=" text-sm">{lang[selectedLanguage].signinBtn}</p></button>
           </div>
        </div>
     ) 
