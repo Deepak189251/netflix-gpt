3f2d6e3d80ba8a8f7eae68178d1d9957c29dfed7
D <deepakkumarbehera2002@gmail.com>
2024-03-21 23:29:57 +0530

    updated landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -25,21 +25,21 @@ import { lang } from "../utils/LanguageConstants";
         <div className=" flex  absolute mt-[250px] justify-center w-[100%]">
           <div className=" text-white  text-center ">
             <div>
-              <h1 className=" text-[43px] font-extrabold">{lang[selectedLanguage].landingHeader}</h1>
-              <p className=" mt-[13px] text-2xl font-normal">{lang[selectedLanguage].landingHeader2}</p>
+              <h1 className=" xl:text-[43px] xl:font-extrabold md:text-[34px] font-bold  sm:text-[26px] text-[22px]">{lang[selectedLanguage].landingHeader}</h1>
+              <p className=" xl:mt-[13px] xl:text-2xl sm:font-normal font-medium mt-[6px] md:text-xl sm:text-lg text-base">{lang[selectedLanguage].landingHeader2}</p>
             </div>
             {/**Laughter. Tears. Thrills. Find it all on Netflix.    ml-[220px] mr-[220px]*/}
-            <div className=" text-white mt-[16px]" >
-              <p className=" text-xl font-normal">{lang[selectedLanguage].landingHeader3}</p>
+            <div className=" text-white xl:mt-[16px] mt-[8px]" >
+              <p className=" xl:text-xl md:text-lg sm:text-base text-sm sm:font-normal font-medium">{lang[selectedLanguage].landingHeader3}</p>
              
               <form className=" flex mt-[13px] justify-center">
              
                  
                  <div className="">
-                    <input type="email" className=" mb-[5px]  bg-black w-[374px] h-[56px] text-white-[20px] border rounded text-left pb-2 pl-3" placeholder="Email address"></input>
+                    <input type="email" className=" mb-[5px]  bg-black xl:w-[374px] xl:h-[56px] md:w-[330px] md:h-[50px] sm:w-[270px] sm:h-[45px] w-[220px] h-[40px] text-white-[20px] border rounded text-left pb-2 pl-3" placeholder="Email address"></input>
                  </div>  
             
-               <Link to={"/signup-form"}><button type="submit" className=" bg-red-600 w-[203px] h-[56px] ml-[10px] text-2xl font-semibold rounded text-left pl-3 flex pt-2 hover:bg-[rgb(193,17,25)] duration-300">{lang[selectedLanguage].signupBtn}<SlArrowRight className=" pl-[10px] pt-[6px] text-3xl font-extrabold"/> </button></Link> 
+               <Link to={"/signup-form"}><button type="submit" className=" bg-red-600 xl:w-[203px] xl:h-[56px] md:h-[50px] md:w-[160px] sm:h-[45px] sm:w-[120px] h-[40px] w-[100px] ml-[10px] xl:text-2xl md:text-xl sm:text-base text-sm md:font-semibold font-medium rounded text-left  flex  hover:bg-[rgb(193,17,25)] duration-300"><span className=" md:ml-[12px] md:mt-[10px] sm:ml-[8px] sm:mt-[8px] ml-[6px] mt-[7px]">{lang[selectedLanguage].signupBtn}</span><span className="md:ml-[10px] md:mt-[15px] sm:ml-[8px] sm:mt-[13px] ml-[5px] mt-[10px]"><SlArrowRight className="  xl:w-[24px] xl:h-[22px] md:w-[18px] md:h-[18px] w-[14px] h-[15px] font-extrabold"/></span> </button></Link> 
               </form>
             </div>
          </div>
