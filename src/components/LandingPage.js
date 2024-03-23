eec42bb189c714bb8fbfbc01c04dee4fe122790d
D <deepakkumarbehera2002@gmail.com>
2024-03-23 23:37:37 +0530

    made responsive to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -16,7 +16,7 @@ import { lang } from "../utils/LanguageConstants";
   
 
     return (
-   <div className="landingpage-container w-[1519px]">
+   <div className="landingpage-container w-[100%]">
      <div className=" bg-center bg-cover w-[100%] h-[630px] bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg')]">
      
       
@@ -25,8 +25,8 @@ import { lang } from "../utils/LanguageConstants";
         <div className=" flex  absolute mt-[250px] justify-center w-[100%]">
           <div className=" text-white  text-center ">
             <div>
-              <h1 className=" xl:text-[43px] xl:font-extrabold md:text-[34px] font-bold  sm:text-[26px] text-[22px]">{lang[selectedLanguage].landingHeader}</h1>
-              <p className=" xl:mt-[13px] xl:text-2xl sm:font-normal font-medium mt-[6px] md:text-xl sm:text-lg text-base">{lang[selectedLanguage].landingHeader2}</p>
+              <h1 className=" xl:text-[43px] xl:font-extrabold md:text-[34px] font-bold  sm:text-[26px] text-[20px]">{lang[selectedLanguage].landingHeader}</h1>
+              <p className=" xl:mt-[13px] xl:text-2xl sm:font-normal font-medium mt-[6px] md:text-xl sm:text-lg text-[15px]">{lang[selectedLanguage].landingHeader2}</p>
             </div>
             {/**Laughter. Tears. Thrills. Find it all on Netflix.    ml-[220px] mr-[220px]*/}
             <div className=" text-white xl:mt-[16px] mt-[8px]" >
@@ -36,10 +36,10 @@ import { lang } from "../utils/LanguageConstants";
              
                  
                  <div className="">
-                    <input type="email" className=" mb-[5px]  bg-black xl:w-[374px] xl:h-[56px] md:w-[330px] md:h-[50px] sm:w-[270px] sm:h-[45px] w-[220px] h-[40px] text-white-[20px] border rounded text-left pb-2 pl-3" placeholder="Email address"></input>
+                    <input type="email" className=" mb-[5px]  bg-black xl:w-[374px] xl:h-[56px] md:w-[330px] md:h-[50px] sm:w-[270px] sm:h-[45px] w-[200px] h-[40px] text-white-[20px] border rounded text-left pb-2 pl-3" placeholder="Email address"></input>
                  </div>  
             
-               <Link to={"/signup-form"}><button type="submit" className=" bg-red-600 xl:w-[203px] xl:h-[56px] md:h-[50px] md:w-[160px] sm:h-[45px] sm:w-[120px] h-[40px] w-[100px] ml-[10px] xl:text-2xl md:text-xl sm:text-base text-sm md:font-semibold font-medium rounded text-left  flex  hover:bg-[rgb(193,17,25)] duration-300"><span className=" md:ml-[12px] md:mt-[10px] sm:ml-[8px] sm:mt-[8px] ml-[6px] mt-[7px]">{lang[selectedLanguage].signupBtn}</span><span className="md:ml-[10px] md:mt-[15px] sm:ml-[8px] sm:mt-[13px] ml-[5px] mt-[10px]"><SlArrowRight className="  xl:w-[24px] xl:h-[22px] md:w-[18px] md:h-[18px] w-[14px] h-[15px] font-extrabold"/></span> </button></Link> 
+               <Link to={"/signup-form"}><button type="submit" className=" bg-red-600 xl:w-[203px] xl:h-[56px] md:h-[50px] md:w-[160px] sm:h-[45px] sm:w-[120px] h-[40px] w-[90px] ml-[10px] xl:text-2xl md:text-xl sm:text-base text-[12px] md:font-semibold font-medium rounded text-left  flex  hover:bg-[rgb(193,17,25)] duration-300"><span className=" md:ml-[12px] md:mt-[10px] sm:ml-[8px] sm:mt-[8px] ml-[6px] mt-[9px]">{lang[selectedLanguage].signupBtn}</span><span className="md:ml-[10px] md:mt-[15px] sm:ml-[8px] sm:mt-[13px] ml-[3px] mt-[12px]"><SlArrowRight className="  xl:w-[24px] xl:h-[22px] md:w-[18px] md:h-[18px] sm:w-[14px] sm:h-[15px] font-extrabold"/></span> </button></Link> 
               </form>
             </div>
          </div>
--- src/components/LandingPageComp1.js
+++ src/components/LandingPageComp1.js
@@ -3,15 +3,16 @@ const LandingPageComp1 = () => {
     return (
         <div className="  text-white">
           <div className="container-comp1 bg-black w-[100%] h-[538px] pt-[50px] pb-[50px]" >
-           <div className=" w-[1061px] h-[438px]  ml-[160px] mr-[160px] flex">
-              <div className=" w-[525px] pr-[12px] pt-[154px] pb-[154px]">
-                 <h1 className=" text-[43px] font-extrabold">Enjoy on your TV</h1>
-                 <p className=" text-[22px]">Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
+           <div className="  md:flex sm:flex-row sm:justify-between md:justify-center ">
+              <div className=" xl:w-[525px] lg:w-[420px] md:w-[330px] md:pr-[12px] md:pt-[154px] md:pb-[154px] mx-[15px] text-center">
+                 <h1 className=" xl:text-[43px] lg:text-[40px] text-[35px]  font-extrabold">Enjoy on your TV</h1>
+                 <p className="lg:text-[22px] md:text-[19px] text-[17px] ">Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
              </div >
-             <div className=" w-[525px] pl-[10px] relative">
-               <img className= " h-[394px] w-[519px] bottom-[19px] z-[1] absolute" alt="tv" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" data-uia="nmhp-card-animation-asset-image"/>
-               <video className=" absolute w-[374px] bottom-[52px] left-[80px] h-[211px] top-[109px]" data-uia="nmhp-card-animation-asset-video" autoPlay muted loop ><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v" type="video/mp4"/></video>
-                 
+             <div className=" xl:w-[525px] lg:w-[420px] md:w-[330px] w-[460px] h-[350px]   md:pl-[10px] lg:bottom-[15px] md:bottom-[8px] bottom-[60px] md:m-0 m-auto  relative text-center ">
+              
+                <img className= " xl:h-[394px] xl:w-[519px] lg:w-[410px] lg:h-[310px] md:w-[320px]  md:h-[250px] sm:w-[440px] sm:h-[320px] w-[320px] h-[250px] xl:top-[25px] top-[90px] bottom-[19px] z-[1] absolute md:left-0 sm:left-0 xs:left-[65px]" alt="tv" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" data-uia="nmhp-card-animation-asset-image"/>
+                <video className=" absolute xl:w-[374px] lg:w-[320px] md:w-[275px] sm:w-[335px] w-[275px]  bottom-[52px] xl:left-[80px] lg:left-[57px] md:left-[42px] left-[55px] xl:h-[211px] lg:h-[174px] md:h-[140px] sm:h-[184px] h-[140px] xl:top-[109px] md:top-[143px] top-[149px]" data-uia="nmhp-card-animation-asset-video" autoPlay muted loop ><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v" type="video/mp4"/></video>
+              
              </div>
            </div> 
           </div>
