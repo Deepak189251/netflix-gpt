6eb0e7b5aceded0e818450bc2da987f4f7df94a1
D <deepakkumarbehera2002@gmail.com>
2024-03-25 23:26:08 +0530

    added custom break points in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '360px',
+      'sm': '520px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px', // Default breakpoints
+      'custom': '1600px', // Custom breakpoint
+    },
     extend: {
      
     },
   },
   plugins: [],
 }
+
