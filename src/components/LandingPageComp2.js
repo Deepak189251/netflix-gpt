48d95a7fb5719341799ff4b8c57ab93bc0154ace
D <deepakkumarbehera2002@gmail.com>
2024-05-08 22:55:12 +0530

    updated responsiveness in landing page component

--- src/components/LandingPageComp2.js
+++ src/components/LandingPageComp2.js
@@ -3,25 +3,25 @@ const LandingPageComp2 = () => {
     return (
         <div className="">
             <div className=" container-comp2 bg-black text-white  w-[100%] h-[538px] pt-[50px] pb-[50px]">
-                <div className=" w-[1061px] h-[438px]  ml-[160px] mr-[160px] flex">
-                    <div className="w-[525px] relative">
-                       <img className=" h-[434px] absolute" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" data-uia="nmhp-card-animation-asset-image" class="default-ltr-cache-1d3w5wq" />
-                       <div className=" w-[290px] h-[100px]  bg-black absolute top-[310px] left-[115px] py-[8px] px-[12px] rounded-md flex border-[2px] border-[rgba(128,128,128,0.7)">
-                           <div className=" h-[84px]">
+                <div className=" w-auto xl:h-[438px] h-[380px] flex justify-center">
+                    <div className=" xl:w-[525px] lg:w-[480px] w-[400px] relative">
+                       <img className=" xl:h-[434px] lg:h-[370px] h-[340px] absolute" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" data-uia="nmhp-card-animation-asset-image"  />
+                       <div className=" xl:w-[290px] xl:h-[100px] lg:w-[255px] lg:h-[85px] w-[220px] h-[75px]  bg-black absolute lg:top-[310px] top-[300px] xl:left-[115px] lg:left-[113px] left-[92px] xl:py-[8px] xl:px-[12px] py-[6px] lg:px-[9px] px-[8px] rounded-md flex border-[2px] border-[rgba(128,128,128,0.7)">
+                           <div className=" xl:h-[84px] lg:h-[70px] h-[60px]">
                               <img className=" w-[100%] h-[100%]" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png"/>
                            </div>
-                           <div className=" ml-[14px] mt-[22px] w-[168px]  h-[40px]"> 
-                              <h1 className=" text-[16px] font-semibold text-white">Stranger Things</h1>
+                           <div className=" xl:ml-[14px] xl:mt-[22px] lg:ml-[15px] lg:mt-[15px] ml-[12px] mt-[13px] xl:w-[168px] h-[40px] lg:w-[115px] w-[110px] "> 
+                              <h1 className=" lg:text-[16px] text-[14px] font-semibold text-white">Stranger Things</h1>
                               <p className=" text-xs text-blue-600">Downloading...</p>
                            </div>
-                           <div className=" w-[48px] h-[50px] mt-[18px]"> 
+                           <div className=" xl:w-[48px] xl:h-[50px] xl:mt-[18px] xl:ml-0 w-[40px] h-[45px] lg:mt-[14px] mt-[9px] lg:ml-[10px] ml-[5px]"> 
                               <img className=" w-[100%] h-[100%] " src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/download-icon.gif" alt="download-icon-gif"/>
                            </div>
                        </div>
                     </div>
-                    <div className="w-[525px] mt-[95px] ">
-                        <h1 className=" text-[43px] font-extrabold text-white">Download your shows <br/> to watch offline</h1>
-                        <p className=" text-[21px] font-medium ">Save your favourites easily and always have something <br/> to watch.</p>
+                    <div className=" xl:w-[525px] lg:w-[480px] w-[390px] xl:mt-[95px] mt-[100px] ">
+                        <h1 className=" xl:text-[43px] lg:text-[39px] text-[33px] lg:font-extrabold font-bold text-white">Download your shows to watch offline</h1>
+                        <p className=" lg:text-[21px] text-[19px] font-medium ">Save your favourites easily and always have something to watch.</p>
                     </div>
                 </div>                
             </div>
