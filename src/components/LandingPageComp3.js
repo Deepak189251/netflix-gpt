cd3826758ba148b4a975f302282d0e89d8a2922c
D <deepakkumarbehera2002@gmail.com>
2024-05-09 22:06:45 +0530

    updated responsiveness of the landingcomponent3

--- src/components/LandingPageComp3.js
+++ src/components/LandingPageComp3.js
@@ -2,15 +2,15 @@ const LandingPageComp3 = () => {
 
     return (
         <div>
-            <div className=" text-white container-comp3 bg-black w-[100%] h-[538px] pt-[50px] pb-[50px]">
-                <div className=" w-[1061px] h-[438px] ml-[160px] mr-[160px] flex">
-                    <div className="w-[525px] h-[168px] pb-[24px] mt-[115px]">
-                        <h1 className="text-[43px] font-extrabold">Watch everywhere</h1>
-                        <p className="text-[21px] font-medium mt-[16px]">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</p>
+            <div className=" text-white container-comp3 bg-black w-[100%] xl:h-[538px] lg:h-[490px] h-[400px] pt-[50px] pb-[50px]">
+                <div className=" w-[auto] xl:h-[438px] lg:h-[390px] h-[300px] flex justify-center">
+                    <div className="sl:w-[525px] lg:w-[460px] w-[340px] lg:h-[168px] h-[154px] pb-[24px] xl:mt-[115px] lg:mt-[80px] mt-[45px]">
+                        <h1 className="xl:text-[43px] lg:text-[40px] text-[35px] font-extrabold">Watch everywhere</h1>
+                        <p className="lg:text-[21px] text-[19px] font-medium mt-[16px]">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</p>
                     </div>
-                    <div className="w-[525px] relative">
-                      <img className=" absolute z-[1]" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png" />
-                      <video className="absolute w-[335px] h-[189px] top-[46px] left-[99px]" data-uia="nmhp-card-animation-asset-video" autoPlay muted loop><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices-in.m4v" type="video/mp4"/></video>
+                    <div className="xl:w-[525px] xl:h-[438px] lg:w-[460px] lg:h-[390px] w-[340px] h-[300px] relative">
+                      <img className=" absolute z-[1] lg:bottom-[53px] bottom-[27px] lg:h-[332px] h-[255px] lg:right-[8px] right-0 top-[18px]" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png" />
+                      <video className="absolute xl:w-[292px] lg:w-[291px] w-[225px] xl:h-[214px] lg:h-[235px] h-[160px] xl:top-[22px] lg:top-0 top-[24px] xl:left-[145px] lg:left-[83px] left-[56px]"  data-uia="nmhp-card-animation-asset-video" autoPlay muted loop><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices-in.m4v" type="video/mp4"/></video>
                     </div>
                 </div>
             </div>
