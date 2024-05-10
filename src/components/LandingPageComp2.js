1e40d1e0a63ad32ea43daf6cabf0fa834f477998
D <deepakkumarbehera2002@gmail.com>
2024-05-10 23:39:30 +0530

    completed responsiveness of comp2 and comp3

--- src/components/LandingPageComp2.js
+++ src/components/LandingPageComp2.js
@@ -2,30 +2,31 @@ const LandingPageComp2 = () => {
 
     return (
         <div className="">
-            <div className=" container-comp2 bg-black text-white  w-[100%] h-[538px] pt-[50px] pb-[50px]">
-                <div className=" w-auto xl:h-[438px] h-[380px] flex justify-center">
-                    <div className=" xl:w-[525px] lg:w-[480px] w-[400px] relative">
-                       <img className=" xl:h-[434px] lg:h-[370px] h-[340px] absolute" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" data-uia="nmhp-card-animation-asset-image"  />
-                       <div className=" xl:w-[290px] xl:h-[100px] lg:w-[255px] lg:h-[85px] w-[220px] h-[75px]  bg-black absolute lg:top-[310px] top-[300px] xl:left-[115px] lg:left-[113px] left-[92px] xl:py-[8px] xl:px-[12px] py-[6px] lg:px-[9px] px-[8px] rounded-md flex border-[2px] border-[rgba(128,128,128,0.7)">
-                           <div className=" xl:h-[84px] lg:h-[70px] h-[60px]">
+            <div className=" container-comp2 bg-black text-white  w-[100%] md:h-[538px] h-[670px] pt-[50px] pb-[50px]">
+                <div className=" w-auto xl:h-[438px] md:h-[380px] h-[570px] flex justify-center md:flex-row items-center flex-col ">
+                    <div className=" xl:w-[525px] lg:w-[480px] md:w-[400px] sm:w-[450px] w-[310px] md:h-[100%] sm:h-[395px] h-[310px] relative">
+                       <img className=" xl:h-[434px] lg:h-[370px] sm:h-[340px] h-[260px] absolute" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" data-uia="nmhp-card-animation-asset-image"  />
+
+                       <div className=" xl:w-[290px] xl:h-[100px] lg:w-[255px] lg:h-[85px] md:w-[220px] sm:w-[244px] sm:h-[75px] w-[174px] h-[58px]  bg-black absolute lg:top-[310px] sm:top-[300px] top-[230px] xl:left-[115px] lg:left-[113px] md:left-[92px] sm:left-[103px] left-[70px] xl:py-[8px] xl:px-[12px] sm:py-[6px] lg:px-[9px] sm:px-[8px] py-[4px] px-[5px] rounded-md flex border-[2px] border-[rgba(128,128,128,0.7)">
+                           <div className=" xl:h-[84px] lg:h-[70px] sm:h-[60px] h-[45px]">
                               <img className=" w-[100%] h-[100%]" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png"/>
                            </div>
-                           <div className=" xl:ml-[14px] xl:mt-[22px] lg:ml-[15px] lg:mt-[15px] ml-[12px] mt-[13px] xl:w-[168px] h-[40px] lg:w-[115px] w-[110px] "> 
-                              <h1 className=" lg:text-[16px] text-[14px] font-semibold text-white">Stranger Things</h1>
+                           <div className=" xl:ml-[14px] xl:mt-[22px] lg:ml-[15px] lg:mt-[15px] md:ml-[12px] md:mt-[13px] ml-[10px] mt-[7px] xl:w-[168px] h-[40px] lg:w-[115px] md:w-[110px] w-[115px] "> 
+                              <h1 className=" lg:text-[16px] md:text-[14px] sm:text-[15px] text-[12px] sm:font-semibold font-medium text-white">Stranger Things</h1>
                               <p className=" text-xs text-blue-600">Downloading...</p>
                            </div>
-                           <div className=" xl:w-[48px] xl:h-[50px] xl:mt-[18px] xl:ml-0 w-[40px] h-[45px] lg:mt-[14px] mt-[9px] lg:ml-[10px] ml-[5px]"> 
+                           <div className=" xl:w-[48px] xl:h-[50px] xl:mt-[18px] xl:ml-0 sm:w-[40px] sm:h-[45px] w-[30px] h-[35px] lg:mt-[14px] md:mt-[9px] mt-[7px] lg:ml-[10px] md:ml-[5px] sm:ml-[8px] ml-0"> 
                               <img className=" w-[100%] h-[100%] " src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/download-icon.gif" alt="download-icon-gif"/>
                            </div>
                        </div>
                     </div>
-                    <div className=" xl:w-[525px] lg:w-[480px] w-[390px] xl:mt-[95px] mt-[100px] ">
-                        <h1 className=" xl:text-[43px] lg:text-[39px] text-[33px] lg:font-extrabold font-bold text-white">Download your shows to watch offline</h1>
+                    <div className=" xl:w-[525px] lg:w-[480px] sm:w-[390px] w-[310px] mt-[10px] md:pr-[5px] pr-0 md:text-left text-center ">
+                        <h1 className=" xl:text-[43px] lg:text-[39px] md:text-[33px] sm:text-[36px] text-[30px] lg:font-extrabold font-bold text-white mb-[10px]">Download your shows to watch offline</h1>
                         <p className=" lg:text-[21px] text-[19px] font-medium ">Save your favourites easily and always have something to watch.</p>
                     </div>
                 </div>                
             </div>
-            <div className=" divider w-[1381px] h-[8px] bg-[rgb(35,35,35)]"></div>
+            <div className=" divider w-[100%] h-[8px] bg-[rgb(35,35,35)]"></div>
         </div>
     )
 
--- src/components/LandingPageComp3.js
+++ src/components/LandingPageComp3.js
@@ -2,19 +2,19 @@ const LandingPageComp3 = () => {
 
     return (
         <div>
-            <div className=" text-white container-comp3 bg-black w-[100%] xl:h-[538px] lg:h-[490px] h-[400px] pt-[50px] pb-[50px]">
-                <div className=" w-[auto] xl:h-[438px] lg:h-[390px] h-[300px] flex justify-center">
-                    <div className="sl:w-[525px] lg:w-[460px] w-[340px] lg:h-[168px] h-[154px] pb-[24px] xl:mt-[115px] lg:mt-[80px] mt-[45px]">
-                        <h1 className="xl:text-[43px] lg:text-[40px] text-[35px] font-extrabold">Watch everywhere</h1>
+            <div className=" text-white container-comp3 bg-black w-[100%] xl:h-[538px] lg:h-[490px] md:h-[400px] sm:h-[560px] h-[520px] pt-[50px] pb-[50px]">
+                <div className=" w-[auto] xl:h-[438px] lg:h-[390px] md:h-[300px] sm:h-[460px] [420px] flex justify-center md:flex-row flex-col items-center">
+                    <div className="sl:w-[525px] lg:w-[460px] md:w-[340px] sm:w-[400px] w-[310px] lg:h-[168px] sm:h-[154px] h-[170px] sm:pb-[24px] pb-[20px] md:text-left text-center">
+                        <h1 className="xl:text-[43px] lg:text-[40px] sm:text-[36px] text-[30px] font-extrabold">Watch everywhere</h1>
                         <p className="lg:text-[21px] text-[19px] font-medium mt-[16px]">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</p>
                     </div>
-                    <div className="xl:w-[525px] xl:h-[438px] lg:w-[460px] lg:h-[390px] w-[340px] h-[300px] relative">
-                      <img className=" absolute z-[1] lg:bottom-[53px] bottom-[27px] lg:h-[332px] h-[255px] lg:right-[8px] right-0 top-[18px]" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png" />
-                      <video className="absolute xl:w-[292px] lg:w-[291px] w-[225px] xl:h-[214px] lg:h-[235px] h-[160px] xl:top-[22px] lg:top-0 top-[24px] xl:left-[145px] lg:left-[83px] left-[56px]"  data-uia="nmhp-card-animation-asset-video" autoPlay muted loop><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices-in.m4v" type="video/mp4"/></video>
+                    <div className="xl:w-[525px] xl:h-[350px] lg:w-[460px] lg:h-[340px] md:w-[340px] md:h-[275px] sm:w-[400px] sm:h-[284px] w-[310px] h-[250px]  relative">
+                      <img className=" absolute z-[1] lg:bottom-[53px] bottom-[27px] lg:h-[332px] md:h-[255px] sm:h-[280px] h-[225px] lg:right-[8px] right-0 sm:top-[18px] top-[27px] xl:w-[460px] lg:w-[441px] sm:w-[400px] w-[307px]" alt="" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png" />
+                      <video className="absolute xl:w-[292px] lg:w-[293px] md:w-[225px] sm:w-[255px] w-[200px] xl:h-[214px] lg:h-[260px] md:h-[160px] sm:h-[172px] h-[167px] xl:top-[22px] lg:top-0 sm:top-[24px] top-[20px] xl:left-[145px] lg:left-[82px] md:left-[56px] sm:left-[71px] left-[53px]"  data-uia="nmhp-card-animation-asset-video" autoPlay muted loop><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices-in.m4v" type="video/mp4"/></video>
                     </div>
                 </div>
             </div>
-            <div className=" divider w-[1381px] h-[8px] bg-[rgb(35,35,35)]"></div>
+            <div className=" divider w-[100%] h-[8px] bg-[rgb(35,35,35)]"></div>
         </div>
     )
 }
