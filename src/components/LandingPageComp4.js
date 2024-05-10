163b5bbd48b781e86458b9604f1abba55f6d0b3a
D <deepakkumarbehera2002@gmail.com>
2024-05-10 23:40:33 +0530

    completed responsiveness of comp4

--- src/components/LandingPageComp4.js
+++ src/components/LandingPageComp4.js
@@ -2,19 +2,19 @@ const LandingPageComp4 = () => {
 
     return(
         <div>
-            <div className="container-comp4 text-white bg-black w-[100%] h-[538px] pt-[50px] pb-[50px]">
-                <div className=" w-[1061px] h-[438px]  ml-[160px] mr-[160px] flex">
-                    <div className="w-[525px]">
-                    <img className=" w-[100%] h-[434px]" src="https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVr8nYuAg0xDpXDv0VI9HUoH7r2aGp4TKRCsKNQrMwxzTtr-NlwOHeS8bCI2oeZddmu3nMYr3j9MjYhHyjBASb1FaOGYZNYvPBCL.png?r=54d" alt=""/>
+            <div className="container-comp4 text-white bg-black w-[100%] xl:h-[538px] lg:h-[500px] md:h-[440px] sm:h-[590px] h-[600px] pt-[50px] pb-[50px]">
+                <div className=" w-[auto] xl:h-[438px] lg:h-[400px] md:h-[340px] sm:h-[490px] h-[500px] flex justify-center items-center md:flex-row flex-col ">
+                    <div className="xl:w-[525px] lg:w-[370px] md:w-[330px] sm:w-[400px] w-[310px] md:mb-0 mb-[10px]">
+                    <img className=" w-[100%] xl:h-[434px] lg:h-[330px] md:h-[300px] sm:h-[330px] h-[260px]" src="https://occ-0-4995-2186.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVr8nYuAg0xDpXDv0VI9HUoH7r2aGp4TKRCsKNQrMwxzTtr-NlwOHeS8bCI2oeZddmu3nMYr3j9MjYhHyjBASb1FaOGYZNYvPBCL.png?r=54d" alt=""/>
                     </div>
-                    <div className="w-[525px] pl-[6px] h-[199px] pb-[24px] mt-[110px]">
-                        <h1 className=" text-[43px] font-extrabold">Create profiles for kids</h1>
+                    <div className="xl:w-[525px] lg:w-[472px] md:w-[420px] sm:w-[440px] w-[310px] md:pl-[6px] pl-0 xl:h-[199px] sm:h-[180px] h-[235px] text-center">
+                        <h1 className=" xl:text-[43px] lg:text-[41px] sm:text-[37px] text-[30px] lg:font-extrabold font-bold ">Create profiles for kids</h1>
                         <p className="text-[21px] font-medium pt-[16px]">Send children on adventures with their favourite characters in a space made just for them—free with your membership.</p>
                     </div>
                 </div>
 
             </div>
-            <div className=" divider w-[1381px] h-[8px] bg-[rgb(35,35,35)]"></div>
+            <div className=" divider w-[100%] h-[8px] bg-[rgb(35,35,35)]"></div>
         </div>
     )
 }
