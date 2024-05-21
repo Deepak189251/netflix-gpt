de066122cf5fde1d03e07fd2d6f92e43da24f65e
D <deepakkumarbehera2002@gmail.com>
2024-05-21 23:31:48 +0530

    updated component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,19 +1,34 @@
 import MovieCard from "./MovieCard"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const MovieList = ({title, movies}) => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 8,
+        slidesToScroll: 2
+      };
 
     //console.log(movies)
     if(!movies) return
 
     return (
         <div className="">
-            <h1 className=" text-[#e5e5e5] ml-[30px] mt-[20px] text-[20px] mb-[20px] font-semibold">{title}</h1>
-            <div className=" overflow-x-scroll mt-[20px] ml-[30px] flex" >
+            
+                <h1 className=" text-[#e5e5e5] ml-[30px] mt-[20px] text-[20px] mb-[20px] font-semibold">{title}</h1>
+                <div className=" overflow-x-scroll  mt-[20px] mx-auto  flex flex-col justify-center" >
                 
-                <div className="flex">
-                {movies.map(movie =>  <MovieCard key={movie.id} poster={movie.poster_path} />)}
+                    <div className="flex">
+                        
+                            {movies.map(movie =>  <MovieCard key={movie.id} id={movie.id} poster={movie.poster_path} />)}
+                        
+                    </div>
+                
                 </div>
-            </div>
+              
         </div>
     )
 }
