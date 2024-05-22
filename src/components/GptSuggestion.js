2eff1a94f7becaf284b55c309c476b9d937c028a
D <deepakkumarbehera2002@gmail.com>
2024-05-22 23:42:54 +0530

    updated gpt suggestion

--- src/components/GptSuggestion.js
+++ src/components/GptSuggestion.js
@@ -3,9 +3,17 @@ import GptMovies from "./GptMovies"
 const GptSuggestion = () => {
 
     return (
-        <div className=" absolute top-[150px] left-[170px]">
+        <div>
+        <div className=" absolute top-[150px] left-0 right-0 m-auto w-[100%]">
+            <div>
             <GptSearch />
+            </div>
+            <div>
             <GptMovies />
+            </div>
+            
+           
+        </div>
         </div>
     )
 }
