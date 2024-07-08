9705b7ba8e38a021b40925d49eb2e4d649a63074
D <deepakkumarbehera2002@gmail.com>
2024-07-08 23:28:42 +0530

    updated design of ui in videobackground

--- src/components/VidoBackground.js
+++ src/components/VidoBackground.js
@@ -1,4 +1,4 @@
-import useNowMovieClips from "../hooks/useNowMovieClips"
+import { useNowMovieClips } from "../hooks/useNowMovieClips"
 import { useSelector } from "react-redux"
 const VideoBackground = ({id}) => {
     useNowMovieClips(id)
@@ -6,11 +6,11 @@ const VideoBackground = ({id}) => {
     if (!Trailer) return
     
     
-    
+    console.log(Trailer.key)
     
     return (
         <div className= " w-[100%] h-[100%] ">
-            <iframe className="w-[100%]  aspect-video" src={`https://www.youtube.com/embed/${Trailer.key}?&autoplay=1&mute=1`} title="YouTube video player"  ></iframe>
+            <iframe className="w-[100%]  aspect-video disabled:" src={`https://www.youtube.com/embed/${Trailer?.key}?&autoplay=1&loop=1&mute=1&playlist=${Trailer?.key}&controls=0&rel=0`} title="YouTube video player"  ></iframe>
         </div>
     )
 }
