42202bd219d846f577d782c6a153d3dcb8e6fe9e
D <deepakkumarbehera2002@gmail.com>
2024-07-13 23:36:50 +0530

    added a new custom hook for selected movie trailer

--- src/hooks/useNowMovieClips.js
+++ src/hooks/useNowMovieClips.js
@@ -1,10 +1,12 @@
 import { useEffect } from "react"
 import { options } from "../utils/Constants"
-import { useDispatch } from "react-redux"
-import { addMovieTrailer } from "../utils/MovieSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { addMovieTrailer, addMovieClip, removeMovieClip } from "../utils/MovieSlice"
 
 
-const useNowMovieClips = (id) => {
+export const useNowMovieClips = (id) => {
+
+    const movieClip = useSelector(store => store.movie.movieTrailer)
 
     const dispatch = useDispatch()
     
@@ -26,7 +28,46 @@ const useNowMovieClips = (id) => {
     }
 
     useEffect(() => {
-        getNowMovieClips()
+        if(!movieClip) getNowMovieClips()
+    }, [])
+
+    
+}
+
+
+export const useMovieClips = (id) => {
+
+    const movieClip = useSelector(store => store.movie.movieClip)
+
+    const dispatch = useDispatch()
+    
+    const getNowMovieClips = async () => {
+
+        const data = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+        const json = await data.json()
+        //console.log(json)
+        const clips = json.results.filter(c => c.type === "Trailer")
+        const trailer = clips.length ? clips[0] : json.results[0]
+        
+        
+        dispatch(addMovieClip(trailer))
+        //console.log(trailer)
+
+
+
+
+    }
+
+    useEffect(() => {
+        if(!movieClip) {
+            getNowMovieClips()
+        }
+        else{
+            if(movieClip.key !== id){
+                dispatch(removeMovieClip())
+                getNowMovieClips()
+            }
+        }
     }, [])
 
     
@@ -34,6 +75,7 @@ const useNowMovieClips = (id) => {
 
 
 
-export default useNowMovieClips
+
+
 
    
