91ef504a8eeb1fb04bc041c147d6dc9db41b096b
D <deepakkumarbehera2002@gmail.com>
2024-07-30 23:04:41 +0530

    used local storage as wishlist

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,8 +1,9 @@
 //import { useNavigate } from "react-router-dom"
-//import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 //import { removeUser } from "../utils/UserSlice"
 //import { UserState } from "../utils/Context"
-//import { useSelector } from "react-redux"
+import { useSelector } from "react-redux"
+import { addMovieInWishList } from "../utils/MovieSlice"
 //import { signOut } from "firebase/auth"
 //import { auth } from "../utils/Firebase"
 //import { nowPlayingUrl, options } from "../utils/Constants"
@@ -12,7 +13,7 @@
  import PrimaryContainer from "./PrimaryContainer"
 import SecondaryContainer from "./SecondaryContainer"
 //import GptSuggestion from "./GptSuggestion"
-import { doc, getDoc } from "firebase/firestore";
+//import { doc, getDoc } from "firebase/firestore";
 
 export const ReadData = async () => {
 
@@ -20,6 +21,9 @@ export const ReadData = async () => {
 }
 
 const Browse = () => {
+    const dispatch = useDispatch()
+    const user = useSelector(store => store?.user?.uid)
+   
    // const navigate = useNavigate()
     //const {state: {users}, dispatch} = UserState()
    // const dispatch = useDispatch()
@@ -31,7 +35,13 @@ const Browse = () => {
    usePopularMovies()
    //const gptValue = useSelector(store => store.gptSearch.gptValue)
     
-   
+   if(!user) return
+   if(!localStorage.getItem(user)) {
+       localStorage.setItem(user, "[]")
+   }
+
+   const wishList = JSON.parse(localStorage.getItem(user))
+   dispatch(addMovieInWishList(wishList))
 
 
     
