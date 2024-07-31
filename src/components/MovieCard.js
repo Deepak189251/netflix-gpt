eff42f4276f28b304f333e4024e6c5e23b742570
D <deepakkumarbehera2002@gmail.com>
2024-07-31 23:17:41 +0530

    updated ui

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,7 +7,7 @@ const MovieCard = ({poster, id}) => {
     
     return (
         <Link to={`/movie/${id}`}>
-            <div className="">
+            <div className=" mb-[15px]">
                 <img className="w-[150px] h-[150px] max-w-[200px] mr-[15px]" alt="poster" src={`https://image.tmdb.org/t/p/w500/${poster}`} />  
             </div>
         </Link>
